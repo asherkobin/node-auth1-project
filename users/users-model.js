30c3db6a59e4afea7ce64870ae3e99a9e14be4da
Asher Kobin <Asher@Ashers-MBP.fios-router.home>
2020-02-25 03:59:23 -0500

    MVP

--- users/users-model.js
+++ users/users-model.js
@@ -4,6 +4,28 @@ async function findAll() {
   return DB("users");
 }
 
+async function findById(id) {
+  return DB("users")
+    .select("id", "username")
+    .where("id", id)
+    .first();
+}
+
+async function findByUsername(username) {
+  return DB("users")
+    .where("username", username)
+    .first();
+}
+
+async function add(userInfo) {
+  const [id] = await DB("users").insert(userInfo);
+
+  return findById(id);
+}
+
 module.exports = {
-  findAll
+  findAll,
+  findById,
+  findByUsername,
+  add
 }
