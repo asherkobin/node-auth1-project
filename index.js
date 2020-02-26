1f7bf2727bf13ff842f7ba5ccdca247e597f1f63
Asher Kobin <Asher@Ashers-MBP.fios-router.home>
2020-02-26 16:15:32 -0500

    Stretch

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const server = express();
 const portNum = process.env.PORT || 5000;
 const authRouter = require("./auth/auth-router.js");
 const usersRouter = require("./users/users-router.js");
+const restrictedRouter = require("./restricted/restricted-router.js");
 const session = require("express-session");
 const KnexStore = require("connect-session-knex")(session);
 const restrictedBySession = require('./auth/session-mw.js');
+
 const sessionConfig = {
   name: "session",
   secret: "secret phrase",
@@ -30,6 +32,7 @@ server.use(session(sessionConfig));
 
 server.use("/api/auth", authRouter);
 server.use("/api/users", restrictedBySession, usersRouter);
+server.use("/api/restricted", restrictedBySession, restrictedRouter);
 
 server.listen(portNum, () => {
   console.log("Express is Running on " + portNum);
